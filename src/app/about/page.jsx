734f57a07cfbf263f8a010288ab144b30eacc7b4
Affan2900 <affanamir290@gmail.com>
2024-12-13 14:31:56 +0500

    initial commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+// src/app/about/page.js
+export default function About() {
+  return (
+    <>
+      <h1>About Us</h1>
+      <p>This is the about page.</p>
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import {motion} from 'framer-motion'
+
+export default function Header(){
+
+  return(
+    <header>
+      <div>
+        
+      </div>
+    </header>
+  )
+}
